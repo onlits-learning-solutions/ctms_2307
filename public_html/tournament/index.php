f902074bad6e608b4d5faf330d03d306960cdd06
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2023-11-01 22:01:07 +0530

    20231101

--- public_html/tournament/index.php
+++ public_html/tournament/index.php
@@ -9,35 +9,65 @@ $tournaments = fetchTournaments();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>CTMS Tournaments</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 </head>
 
-<body>
-    <h1>List of Tournaments</h1>
-    <table border="1">
-        <thead>
-            <tr>
-                <th>Id</th>
-                <th>Name</th>
-                <th>Venue</th>
-                <th>Organiser</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            foreach ($tournaments as $tournament) {
-            ?>
-            <tr>
-                <td><?= $tournament['id'] ?></td>
-                <td><?= $tournament['name'] ?></td>
-                <td><?= $tournament['venue'] ?></td>
-                <td><?= $tournament['organiser'] ?></td>
-            </tr>
-            <?php
-            }
-            ?>
-        </tbody>
-    </table>
+<body class="container mt-3">
+    <header>
+        <h1>CTMS 1.0</h1>
+    </header>
+    <nav class="navbar bg-light navbar-light">
+        <div class="container-fluid">
+            <ul class="navbar-nav">
+                <li class="navbar-item">
+                    <a href="index.php" class="nav-link">Home</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav">
+                <li class="navbar-item">
+                    <a href="./tournament/index.php" class="nav-link">Tournament</a>
+                </li>
+            </ul>
+
+            <ul class="navbar-nav">
+                <li class="navbar-item">
+                    <a href="./player/index.php" class="nav-link">Player</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <main>
+        <h3 class="mt-3">List of Tournaments</h3>
+        <a class="btn btn-primary mt-3 mb-3" href="new.php">New Tournament</a>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Venue</th>
+                    <th>Organiser</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($tournaments as $tournament) {
+                ?>
+                    <tr>
+                        <td><?= $tournament['id'] ?></td>
+                        <td><?= $tournament['name'] ?></td>
+                        <td><?= $tournament['venue'] ?></td>
+                        <td><?= $tournament['organiser'] ?></td>
+                    </tr>
+                <?php
+                }
+                ?>
+            </tbody>
+        </table>
+    </main>
+    <footer>
+
+    </footer>
 </body>
 
 </html>
--- public_html/tournament/new.php
+++ public_html/tournament/new.php
@@ -1,17 +1,21 @@
+<?php
+    include  'create.php';
+    createTournament($_POST);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CTMS Home</title>
+    <title>New Tournament</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-
 </head>
 
-<body class="container mt-3">
+<body class="container">
     <header>
-        <h1>CTMS 1.0</h1>
+        <h1 class="mt-3">CTMS 1.0</h1>
     </header>
     <nav class="navbar bg-light navbar-light">
         <div class="container-fluid">
@@ -22,7 +26,7 @@
             </ul>
             <ul class="navbar-nav">
                 <li class="navbar-item">
-                    <a href="./tournament/index.php" class="nav-link">Tournament</a>
+                    <a href="index.php" class="nav-link">Tournament</a>
                 </li>
             </ul>
 
@@ -33,8 +37,18 @@
             </ul>
         </div>
     </nav>
-    <main>
 
+    <main>
+        <h3 class="mt-3 mb-3">New Tournament</h3>
+        <form method="post">
+            <label for="name" class="form-label">Name</label>
+            <input type="text" name="name" id="name" class="form-control">
+            <label for="venue" class="form-label mt-3">Venue</label>
+            <input type="text" name="venue" id="venue" class="form-control">
+            <label for="organizer" class="form-label mt-3">Organizer</label>
+            <input type="text" name="organizer" id="organizer" class="form-control">
+            <button class="btn btn-primary mt-3 mb-5">Submit</button>
+        </form>
     </main>
     <footer>
         <p>&copy;2023, Onlits Learning Solutions</p>
